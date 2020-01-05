131f842ac2c01f5a03dd978eb2f3b2b95bb87851
Paffcio <pawel0ploszaj@gmail.com>
2020-01-05 23:49:27 +0100

    Fix

--- main.c
+++ main.c
@@ -1,8 +1,18 @@
-#include<stdblib.h>
+#include<stdio.h>
+#include<stdlib.h>
+#include<math.h>
+#include "konwerter.c"
+#include "Negacja.c"
+#include "Znak.c"
+#include "Start.c"
 int main()
 {
-int a;
-scanf("%d",&a);
-printf("Podales liczbe:%d",a);
+int t,p;
+char o;
+scanf("%d",&t);
+scanf("%d",&p);
+scanf("%s",&o);
+znak(o);
+start(t,p);
 return 0;
 }
