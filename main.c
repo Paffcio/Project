915db30981f258f5961c1206e0ea1d0310bcf5c8
Paffcio <pawel0ploszaj@gmail.com>
2020-01-06 02:25:31 +0100

    Calculator

--- main.c
+++ main.c
@@ -1,18 +1,42 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<math.h>
-#include "konwerter.c"
-#include "Negacja.c"
-#include "Znak.c"
-#include "Start.c"
+#include "dodawanie.h"
+#include "odejmowanie.h"
+#include "mnozenie.h"
+#include "dzielenie.h"
 int main()
 {
-int t,p;
-char o;
-scanf("%d",&t);
-scanf("%d",&p);
-scanf("%s",&o);
-znak(o);
-start(t,p);
+double x=0.0;
+double y=0.0;
+char operator;
+printf("                           Prosty kalkulator                    \n\n");
+printf("Podaj pierwsza liczbe:");
+scanf("%lf",&x);
+printf("\n");
+printf("Podaj znak operacji:");
+scanf("%lf",&y);
+printf("\n");
+printf("Podaj druga liczbe:");
+scanf("%s",&operator);
+printf("\n");
+switch(operator)
+{
+case '+':
+dodaj(x,y);
+break;
+case '-'
+odejmij(x,y);
+break;
+case '*':
+pomnoz(x,y);
+break;
+case '/':
+podziel(x,y);
+break;
+default:
+printf("Bledny znak\n");
+break;
+}
 return 0;
 }
